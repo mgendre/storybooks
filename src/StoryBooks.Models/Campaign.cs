d2b7a8dc30872a93d90432fe069ed2c3b614d982
Michel Gendre <michel.gendre@softcom.pro>
2021-08-19 20:53:56 +0200

    Scenarios component

--- src/StoryBooks.Models/Campaign.cs
+++ src/StoryBooks.Models/Campaign.cs
@@ -16,7 +16,7 @@ namespace StoryBooks.Models
         [JsonConverter(typeof(StringEnumConverter))]
         public CampaignStatus Status { get; set; }
         
-        public Scenario[] Scenarios { get; set; }
+        public Scenario[] Scenarios { get; set; } = Array.Empty<Scenario>();
         
         public DateTime CreationDate { get; set; }
         
